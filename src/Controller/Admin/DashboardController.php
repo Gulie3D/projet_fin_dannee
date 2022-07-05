1413597c883e25cb4205e281b1925a1c80d698a6
julie causse <julie-causse@live.fr>
2022-07-05 15:27:06 +0200

    ajout des header dans easyAdmin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Carrier;
 use App\Entity\Category;
+use App\Entity\Header;
 use App\Entity\Order;
 use App\Entity\Product;
 use App\Entity\User;
@@ -56,5 +57,6 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToCrud('Produits', 'fas fa-tag', Product::class);
         yield MenuItem::linkToCrud('Transporteurs', 'fas fa-truck', Carrier::class);
         yield MenuItem::linkToCrud('Commandes', 'fas fa-box', Order::class);
+        yield MenuItem::linkToCrud('Headers', 'fas fa-desktop', Header::class);
     }
 }
